ce2cccbc57162723f1d4060d0e451033fc276dbe
rainnfx <matteo.aure@gmail.com>
2024-11-03 17:27:03 -0800

    uh

---
+++ src/app/writing/[...slug]/page.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Metadata } from "next";
+import { writing as allBlogs } from "#site/content";
+import { cn, formatDate } from "@/lib/utils";
+import "@/styles/mdx.css";
+
+import Image from "next/image";
+import { siteConfig } from "@/config/site";
+import { Mdx } from "@/components/mdx-component";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+interface BlogPageItemProps {
+  params: {
+    slug: string[];
+  };
+}
+
+async function getBlogFromParams(params: BlogPageItemProps["params"]) {
+  const slug = params?.slug.join("/");
+  const blog = allBlogs.find((blog) => blog.slugAsParams === slug);
+
+  if (!blog) {
+    return null;
+  }
+
+  return blog;
+}
+
+export async function generateMetadata({
+  params,
+}: BlogPageItemProps): Promise<Metadata> {
+  const blog = await getBlogFromParams(params);
+
+  if (!blog) {
+    return {};
+  }
+
+  return {
+    title: blog.title,
+    description: blog.description,
+    authors: {
+      name: blog.author,
+    },
+  };
+}
+
+export async function generateStaticParams(): Promise<
+  BlogPageItemProps["params"][]
+> {
+  return allBlogs.map((blog) => ({
+    slug: blog.slugAsParams.split("/"),
+  }));
+}
+
+export default async function BlogPageItem({ params }: BlogPageItemProps) {
+  const blog = await getBlogFromParams(params);
+
+  if (!blog) {
+    return {};
+  }
+
+  return (
+    <article className="container relative max-w-3xl py-6 lg:py-10">
+      <div>
+        {blog.date && (
+          <time
+            dateTime={blog.date}
+            className="block text-sm text-muted-foreground -mb-5"
+          >
+            Written by {blog.author} on {formatDate(blog.date)}
+          </time>
+        )}
+
+        {blog.image ? (
+          <Image
+            src={blog.image}
+            alt={blog.title}
+            width={720}
+            height={405}
+            priority
+            className="my-8 border bg-muted transition-colors"
+          />
+        ) : (
+          <div className="my-8" /> // This empty div adds spacing for when there's no image
+        )}
+
+        <h1 className="-mt-2 inline-block text-4xl font-bold capitalize leading-tight text-primary lg:text-5xl">
+          {blog.title}
+        </h1>
+        <hr className="mt-5 mb-7" />
+        <Mdx code={blog.body} />
+        <hr className="mt-12" />
+        <div className="flex justify-center py-6 lg:py-10">
+          <Link
+            href="/posts"
+            className={cn(buttonVariants({ variant: "ghost" }))}
+          >
+            <ChevronLeft className="mr-2 size-4" />
+            See all Posts
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+}
