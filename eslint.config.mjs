4c1fbd6a352a1f95b03a27f9087be4ccef503497
rainnfx <matteo.aure@gmail.com>
2024-12-19 16:54:10 -0800

    ello

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next"],
+    rules: {},
+    settings: {
+      tailwindcss: {
+        config: path.join(__dirname, "./tailwind.config.ts"),
+      },
+    },
+  }),
+];
+
+export default eslintConfig;
